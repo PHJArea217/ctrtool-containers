136b5fc51c048c5f8918b2bc2846aeed079d1ff0
Peter H. Jin <peter@peterjin.org>
2022-08-20 18:05:02 -0400

    Add 2layer snippets

--- 2layer/make-2layer.py
+++ 2layer/make-2layer.py
@@ -34,8 +34,21 @@ os.mkdir(install_dir + "/rootfs/_root", mode=0o755)
 os.chown(install_dir + "/rootfs/_root", root_uid, root_gid)
 
 ctrtool_path = extra_args['ctrtool'] if 'ctrtool' in extra_args else 'ctrtool'
+selected_snippets = extra_args['snippets'].split(',') if 'snippets' in extra_args else []
+flags = extra_args['flags'].split(',') if 'flags' in extra_args else []
+if 'netns_ambient' in flags:
+    netns_switch = 'unshare -n'
+    netns_flag = ''
+else:
+    netns_switch = '--net="/proc/self/fd/$2/ns/net"'
+    netns_flag = 'n'
+if 'netns_veth_name' in extra_args:
+    netns_veth = f'ip link add name "{netns_veth_name}" type veth peer name eth0 netns /proc/self/fd/"$2"/root/proc/driver/run/netns/__host__\n'
+else:
+    netns_veth = ''
 
-subprocess.run([ctrtool_path, 'launcher', '-U', '--escape', '--uid-map=' + uid_map, '--gid-map=' + gid_map, '-sw', 'bsdtar', '-xC', install_dir + '/rootfs/_root', '-f', rootfs_tar], check=True)
+if rootfs_tar != "/dev/null":
+    subprocess.run([ctrtool_path, 'launcher', '-U', '--escape', '--uid-map=' + uid_map, '--gid-map=' + gid_map, '-sw', 'bsdtar', '-xC', install_dir + '/rootfs/_root', '-f', rootfs_tar], check=True)
 
 install_dir_realpath = os.path.realpath(install_dir)
 
@@ -45,10 +58,10 @@ import os
 ctrtool = """''' + ctrtool_path + '''"""
 os.environ['CTRTOOL'] = ctrtool
 os.execv(ctrtool, ['launcher', '-U', '--escape', '--uid-map=''' + uid_map + '''', '--gid-map=''' + gid_map + '''', '-s', '-w', '--script-is-shell', '-V', """--script=/bin/true;set -eu
-nsenter --user="/proc/self/fd/$2/ns/user" --net="/proc/self/fd/$2/ns/net" --ipc="/proc/self/fd/$2/ns/ipc" --mount="/proc/self/fd/$2/ns/mnt" sh -c 'set -eu
+nsenter --user="/proc/self/fd/$2/ns/user" --ipc="/proc/self/fd/$2/ns/ipc" --mount="/proc/self/fd/$2/ns/mnt" ''' + netns_switch + ''' sh -c 'set -eu
 "$CTRTOOL" rootfs-mount -o root_link_opts=all_rw -o mount_sysfs=1 /proc/driver
-"$CTRTOOL" mount_seq -c /proc/driver -m _fsroot_rw -E -s "$1/rootfs/_root" -Obv
+"$CTRTOOL" mount_seq -c /proc/driver -m _fsroot_rw -E -s "$1/rootfs/_root" -Obv -D run/netns -M 0755 -m run/netns/__host__ -f -s /proc/self/ns/net -K -Ob
 ' _ ''' + "'" + install_dir_realpath + "'" + '''
-""", '--alloc-tty', '--wait', '-nmpCiu', '--hostname=my-ctrtool-container', '-t', '-r/proc/driver', '/bin/bash'])
+""", '--alloc-tty', '--wait', '-mpCiu''' + netns_flag + '''', '--hostname=my-ctrtool-container', '-t', '-r/proc/driver', '/bin/bash'])
 ''')
     os.fchmod(start_script.fileno(), 0o755)
