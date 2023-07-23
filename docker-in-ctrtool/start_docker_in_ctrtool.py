235aedac1dd177b7e191686b566e9ce8300bf1b8
Peter H. Jin <peter@peterjin.org>
2023-07-23 14:36:48 -0400

    more 2

--- docker-in-ctrtool/start_docker_in_ctrtool.py
+++ docker-in-ctrtool/start_docker_in_ctrtool.py
@@ -23,6 +23,7 @@ if args.setup == True:
         "route_ipv6": ["2001:db8:0:1:300::/72"],
         "ipv4": ["172.20.0.1"],
         "ipv6": ["2001:db8:0:1:300::1"],
+        "net_iface_ip": ["172.19.255.1"],
         "net_iface": "vif0",
         "hostname": "docker-in-ctrtool"
 }''')
@@ -45,7 +46,6 @@ real_rundir = args.run_directory + '/g-' + args.group
 os.setenv('D_IN_C_DIR', real_directory)
 os.setenv('D_IN_C_CGDIR', real_cgdir)
 os.setenv('D_IN_C_RUNDIR', real_rundir)
-networks = []
 config = json.load(open(real_directory + '/config.json', 'r'))
 if config['configured'] != True:
     sys.stderr.write(f'{real_directory} not configured\n')
@@ -58,6 +58,8 @@ os.mkdir(real_cgdir, mode=0o777)
 os.chown(real_cgdir, config['root_uid'], config['root_gid'])
 os.chown(real_cgdir + '/cgroup.procs', config['root_uid'], config['root_gid'])
 os.chown(real_cgdir + '/cgroup.subtree_control', config['root_uid'], config['root_gid'])
+networks = []
+local_ip = []
 for n in config['ipv4']:
     networks.append(ipaddress.IPv4Network(n))
 for n in config['route_ipv4']:
@@ -66,12 +68,21 @@ for n in config['ipv6']:
     networks.append(ipaddress.IPv6Network(n))
 for n in config['route_ipv6']:
     networks.append(ipaddress.IPv6Network(n))
+for n in config['net_iface_ip']:
+    local_ip.append(ipaddress.ip_address(n))
 os.setenv('D_IN_C_IFACE', config['net_iface'])
 os.setenv('D_IN_C_NETWORKS', ' '.join(str(n) for n in networks))
-os.execvp('ctrtool', ['ctrtool', 'launcher', '-U', '--escape', '--uid-map=' + config['uid_map'], '--gid-map=' + config['gid_map'], '-Cimnpu', '--hostname=' + config['hostname'],f'--write-pid={real_cgdir}/cgroup.procs', '--script-is-shell', '''--script=/bin/true;set -eu
+os.setenv('D_IN_C_LOCAL', ' '.join(str(n) for n in local_ip))
+os.execvp('ctrtool', ['ctrtool', 'launcher', '-U', '--escape', '--alloc-tty', '--uid-map=' + config['uid_map'], '--gid-map=' + config['gid_map'], '-Cimnpu', '--hostname=' + config['hostname'],f'--write-pid={real_cgdir}/cgroup.procs', '--script-is-shell', '''--script=/bin/true;set -eu
 cd "/proc/self/fd/$2/ns"
-nsenter --user=user --ipc=ipc --mount=mnt --net=net
-
-                      ip link add name "$D_IN_C_IFACE" type veth peer name eth0 netns "/proc/self/fd/$2/ns/net"
-                      for n in $D_IN_C_NETWORKS;do ip route add "$n" via inet6 "fe80::2" dev "$D_IN_C_IFACE"
-args.directory = 
+nsenter --user=user --ipc=ipc --mount=mnt --net=net sh -eu -c '
+ctrtool rootfs-mount -o root_link_opts=usr_ro -o root_symlink_usr=1 -o mount_sysfs=1 /proc/driver
+cd /proc/driver
+ctrtool mount_seq -m "_fsroot_rw" -E -s "$D_IN_C_DIR/rootfs/_root" -Obv -D _fsroot_ro -M 0755 -m "_fsroot_ro/usr" -E -s /usr -Obv
+'
+ip link add name "$D_IN_C_IFACE" type veth peer name eth0 netns "/proc/self/fd/$2/ns/net"
+for n in $D_IN_C_LOCAL fe80::1; do ip addr add "$n" dev "$D_IN_C_IFACE"; done
+ip link set dev "$D_IN_C_IFACE" up
+for n in $D_IN_C_NETWORKS;do ip route add "$n" via inet6 "fe80::2" dev "$D_IN_C_IFACE";done
+                      ''', '-V', '--mount-proc', '--pivot-root=/proc/driver', '/bin/bash'])
+# args.directory = 
