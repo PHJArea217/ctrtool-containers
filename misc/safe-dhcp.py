6227a7aebb4b00ca4f05b09593d5cd217c833cf3
Peter H. Jin <peter@peterjin.org>
2023-08-12 22:59:05 -0400

    safe-dhcp: Make no-op if $old_* == $new_*

--- misc/safe-dhcp.py
+++ misc/safe-dhcp.py
@@ -42,26 +42,31 @@ def run_cmd(cmd, cmd_args):
     else:
         cmd_queue.append(['/bin/sh', '-c', cmd, '_'] + cmd_args)
 
+def is_same_environ(key):
+    return os.environ.get('old_' + key, '')==os.environ.get('new_' + key, '')
 def change_ips(old=False, new=False):
-    if old and 'old_routers' in os.environ:
-        safe_v4 = get_safe_ipv4(str(os.environ['old_routers']).split(' ')[0])
-        run_cmd('set -eu; GW="${1%%/*}"; shift 1; ip route del 0.0.0.0/0 via "$GW" proto dhcp dev "$interface" "$@" || :', [safe_v4] + table_vrf)
-    if old and 'old_ip_address' in os.environ:
-        safe_v4 = get_safe_ipv4(os.environ['old_ip_address'], mask=os.environ.get('old_subnet_mask', '255.255.255.255'))
-        run_cmd('ip -4 addr del "$1" dev "$interface" || :', [safe_v4])
-    if new and 'new_ip_address' in os.environ:
-        safe_v4 = get_safe_ipv4(os.environ['new_ip_address'], mask=os.environ.get('new_subnet_mask', '255.255.255.255'))
-        run_cmd('ip -4 addr add "$1" dev "$interface"', [safe_v4])
-    if new and 'new_routers' in os.environ:
-        safe_v4 = get_safe_ipv4(str(os.environ['new_routers']).split(' ')[0])
-        run_cmd('set -eu; GW="${1%%/*}"; shift 1; ip route add 0.0.0.0/0 via "$GW" proto dhcp dev "$interface" "$@"', [safe_v4] + table_vrf)
+    if not is_same_environ('routers'):
+        if old and 'old_routers' in os.environ:
+            safe_v4 = get_safe_ipv4(str(os.environ['old_routers']).split(' ')[0])
+            run_cmd('set -eu; GW="${1%%/*}"; shift 1; ip route del 0.0.0.0/0 via "$GW" proto dhcp dev "$interface" "$@" || :', [safe_v4] + table_vrf)
+        if new and 'new_routers' in os.environ:
+            safe_v4 = get_safe_ipv4(str(os.environ['new_routers']).split(' ')[0])
+            run_cmd('set -eu; GW="${1%%/*}"; shift 1; ip route add 0.0.0.0/0 via "$GW" proto dhcp dev "$interface" "$@"', [safe_v4] + table_vrf)
+    if not is_same_environ('ip_address'):
+        if old and 'old_ip_address' in os.environ:
+            safe_v4 = get_safe_ipv4(os.environ['old_ip_address'], mask=os.environ.get('old_subnet_mask', '255.255.255.255'))
+            run_cmd('ip -4 addr del "$1" dev "$interface" || :', [safe_v4])
+        if new and 'new_ip_address' in os.environ:
+            safe_v4 = get_safe_ipv4(os.environ['new_ip_address'], mask=os.environ.get('new_subnet_mask', '255.255.255.255'))
+            run_cmd('ip -4 addr add "$1" dev "$interface"', [safe_v4])
 def change_ipv6(old=False, new=False):
-    if old and 'old_ip6_address' in os.environ:
-        safe_v4 = get_safe_ipv6(str(os.environ['old_ip6_address']).split(' ')[0]) # ,mask=64
-        run_cmd('ip -6 addr del "$1" dev "$interface" || :', [safe_v4])
-    if new and 'new_ip6_address' in os.environ:
-        safe_v4 = get_safe_ipv6(str(os.environ['new_ip6_address']).split(' ')[0])
-        run_cmd('ip -6 addr add "$1" dev "$interface"', [safe_v4])
+    if not is_same_environ('ip6_address'):
+        if old and 'old_ip6_address' in os.environ:
+            safe_v4 = get_safe_ipv6(str(os.environ['old_ip6_address']).split(' ')[0]) # ,mask=64
+            run_cmd('ip -6 addr del "$1" dev "$interface" || :', [safe_v4])
+        if new and 'new_ip6_address' in os.environ:
+            safe_v4 = get_safe_ipv6(str(os.environ['new_ip6_address']).split(' ')[0])
+            run_cmd('ip -6 addr add "$1" dev "$interface"', [safe_v4])
 def gen_pd_offset(base, displacement, required_mask):
     base_split = str(base).split('/')
     mask = int(base_split[1])
@@ -72,22 +77,23 @@ def gen_pd_offset(base, displacement, required_mask):
         return ipaddress.IPv6Address(first_val + displacement)
     raise ArgumentError(f'IPv6 prefix delegation size (e: {required_mask}, g: {mask}) not expected')
 def change_ipv6_pd(old=False, new=False):
-    if old and 'old_ip6_prefix' in os.environ:
-        old_v6 = str(os.environ['old_ip6_prefix']).split(' ')[0]
-        # if_1 = gen_pd_offset(old_v6, 0x300000000000001, 60)
-        # if_2 = gen_pd_offset(old_v6, 0x10300000000000001, 60)
-        # if_3 = gen_pd_offset(old_v6, 0x20300000000000000, 60)
-        # run_cmd('ip addr del "$1/64" dev lower_eth1 || :', [str(if_1)])
-        # run_cmd('ip addr del "$1/64" dev lower_eth2 || :', [str(if_2)])
-        # run_cmd('ip6tables -t nat -D POSTROUTING -s "2001:db8:1000::/40" -o eth0 -j NETMAP --to "$1/72" || :', [str(if_3)])
-    if new and 'new_ip6_prefix' in os.environ:
-        new_v6 = str(os.environ['new_ip6_prefix']).split(' ')[0]
-        # if_1 = gen_pd_offset(new_v6, 0x300000000000001, 60)
-        # if_2 = gen_pd_offset(new_v6, 0x10300000000000001, 60)
-        # if_3 = gen_pd_offset(new_v6, 0x20300000000000000, 60)
-        # run_cmd('ip addr add "$1/64" dev lower_eth1 || :', [str(if_1)])
-        # run_cmd('ip addr add "$1/64" dev lower_eth2 || :', [str(if_2)])
-        # run_cmd('ip6tables -t nat -I POSTROUTING -s "2001:db8:1000::/40" -o eth0 -j NETMAP --to "$1/72" || :', [str(if_3)])
+    if not is_same_environ('ip6_prefix'):
+        if old and 'old_ip6_prefix' in os.environ:
+            old_v6 = str(os.environ['old_ip6_prefix']).split(' ')[0]
+            # if_1 = gen_pd_offset(old_v6, 0x300000000000001, 60)
+            # if_2 = gen_pd_offset(old_v6, 0x10300000000000001, 60)
+            # if_3 = gen_pd_offset(old_v6, 0x20300000000000000, 60)
+            # run_cmd('ip addr del "$1/64" dev lower_eth1 || :', [str(if_1)])
+            # run_cmd('ip addr del "$1/64" dev lower_eth2 || :', [str(if_2)])
+            # run_cmd('ip6tables -t nat -D POSTROUTING -s "2001:db8:1000::/40" -o eth0 -j NETMAP --to "$1/72" || :', [str(if_3)])
+        if new and 'new_ip6_prefix' in os.environ:
+            new_v6 = str(os.environ['new_ip6_prefix']).split(' ')[0]
+            # if_1 = gen_pd_offset(new_v6, 0x300000000000001, 60)
+            # if_2 = gen_pd_offset(new_v6, 0x10300000000000001, 60)
+            # if_3 = gen_pd_offset(new_v6, 0x20300000000000000, 60)
+            # run_cmd('ip addr add "$1/64" dev lower_eth1 || :', [str(if_1)])
+            # run_cmd('ip addr add "$1/64" dev lower_eth2 || :', [str(if_2)])
+            # run_cmd('ip6tables -t nat -I POSTROUTING -s "2001:db8:1000::/40" -o eth0 -j NETMAP --to "$1/72" || :', [str(if_3)])
 def set_dns():
     if 'new_domain_name_servers' in os.environ:
         safe_v4 = get_safe_ipv4(str(os.environ['new_domain_name_servers']).split(' ')[0])
